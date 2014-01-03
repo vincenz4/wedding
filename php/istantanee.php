1e7f3713e31f503358eb3c15953e046b2a619f88
Francesco Castellani <castellani.f@gmail.com>
2014-01-03 16:49:01 +0100

    Versione mobile e istantanee

--- php/istantanee.php
+++ php/istantanee.php
@@ -5,5 +5,53 @@
         echo $h2o->render(array('DEBUG_ENABLE'=>False,
         	'charset' => 'UTF-8',
         	'pagina' => 'istantanee',
+        	'album1_thumb' => './static/img/fedi-tr.png',
+        	'album1' => array( 
+				array(  
+					'file' =>  './static/img/vincenza/54.jpg',
+					'backgroundShiftX' =>  '-38px',
+					'backgroundShiftY' =>  '0px',
+					'titoloThumb' => 'in bicicletta',
+					'titoloSlide' => 'in bicicletta',
+				),
+				array(  
+					'file' =>  './static/img/vincenza/46.jpg',
+					'backgroundShiftX' =>  '-38px',
+					'backgroundShiftY' =>  '0px',
+					'titoloThumb' => 'Un pò di vanità',
+					'titoloSlide' => 'Un pò di vanità',
+				),
+				array(  
+					'file' =>  './static/img/vincenza/49.jpg',
+					'backgroundShiftX' =>  '-38px',
+					'backgroundShiftY' =>  '0px',
+					'titoloThumb' => 'La meditazione',
+					'titoloSlide' => 'La meditazione',
+				)
+			),
+        	'album2_thumb' => './static/img/fedi-tr.png',
+ 			'album2' => array( 
+				array(  
+					'file' =>  './static/img/francesco/22.jpg',
+					'backgroundShiftX' =>  '-38px',
+					'backgroundShiftY' =>  '0px',
+					'titoloThumb' => 'La prima mi era andata bene ... la seconda no',
+					'titoloSlide' => 'La prima mi era andata bene ... la seconda no',
+				),
+				array(  
+					'file' =>  './static/img/francesco/43.jpg',
+					'backgroundShiftX' =>  '-38px',
+					'backgroundShiftY' =>  '0px',
+					'titoloThumb' => 'Smorfia',
+					'titoloSlide' => 'Smorfia',
+				),
+				array(  
+					'file' =>  './static/img/francesco/14.jpg',
+					'backgroundShiftX' =>  '-38px',
+					'backgroundShiftY' =>  '0px',
+					'titoloThumb' => 'L\'adone',
+					'titoloSlide' => 'L\'adone',
+				)
+			),
         ));
 ?>
